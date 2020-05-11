cd2f4f75da49152e24a97cf037b8f56a88c35e48
Vissu <viswanadh.adapala.org@gmail.com>
2020-05-11 14:40:23 +0530

    commit for changes in admin.py file and adding querying documents

--- Django2.0.7_Py3.6.5/Dlearning/Practice/admin.py
+++ Django2.0.7_Py3.6.5/Dlearning/Practice/admin.py
@@ -2,8 +2,13 @@ from django.contrib import admin
 from Practice.models import Language, Framework
 from Practice.models import Person, Address, Musician, Album, Customer, Topping, Pizza, Employe, Techgroup, Onboard, Student, People, Mypeople, Blog, Author, Entry
 
+class frameadmin(admin.ModelAdmin):
+    list_display = ['frame_name', 'frame_lang']
+    list_filter = ['frame_lang',]
+    #search_fields = ['frame_lang', 'frame_name']
+    #prepopulated_fields = {'slug': ('frame_lang','frame_name',)}
 admin.site.register(Language)
-admin.site.register(Framework)
+admin.site.register(Framework, frameadmin)
 
 # Register your models here.
 # admin.site.register(Person)
@@ -19,6 +24,14 @@ admin.site.register(Framework)
 # admin.site.register(Student)
 # admin.site.register(People)
 # admin.site.register(Mypeople)
+
+class Entryadmin(admin.ModelAdmin):
+    list_display = ['headline', 'blog', 'pub_date', 'mod_date']
+    list_filter = ['blog', 'pub_date', 'mod_date']
+    list_display_links = ['headline','blog']
+    #list_select_related = ['blog']
+    search_fields = ['headline', 'blog']
+    
 admin.site.register(Blog)
 admin.site.register(Author)
-admin.site.register(Entry)
+admin.site.register(Entry, Entryadmin)
